6913842ccbfe1c3961a9d82b6706a4828392b58f
alparslanKobak <alparslankobak@gmail.com>
2023-05-01 23:06:12 +0300

    Add project files.

---
+++ P013EStore.Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace P013EStore.Core.Entities
+{
+    public interface IEntity
+    {
+        // internal interface'yi Public yaptık
+
+        public int Id { get; set; }
+
+       
+    }
+}
